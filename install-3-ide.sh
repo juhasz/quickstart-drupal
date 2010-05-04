7a76807f2278275c31a5772a6d68f91cdf16d4c3
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2010-05-04 17:06:08 +0000

    Gimp and inkscape too heavy... :-(

--- install-3-ide.sh
+++ install-3-ide.sh
@@ -12,10 +12,10 @@ bash ./netbeans.sh
 rm netbeans.sh
 cd ~
 
-# install graphics editors
-sudo aptitude -y install inkscape gimp
+# install graphics editors - Nice idea, but weights about 200mb
+#sudo aptitude -y install inkscape gimp
 
-# install graphical version control
+# install graphical version control - weighs about 70mb
 sudo add-apt-repository ppa:rabbitvcs/ppa && sudo aptitude update
 sudo aptitude -y install rabbitvcs-nautilus
 sudo killall nautilus
