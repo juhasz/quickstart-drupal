9366c2722b31d8a2d3ada23ba65068ae0fdbcff7
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2010-05-06 19:43:02 +0000

    fix repository url

--- install-1-prep.sh
+++ install-1-prep.sh
@@ -8,7 +8,7 @@ iface eth0 inet dhcp" | sudo tee -a /etc/network/interfaces
 sudo /etc/init.d/networking restart
 
 # configure repositories for git-core, etc
-sudo add-apt-repository "deb http://archive.ubuntu.com/ lucid partner"
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ lucid partner"
 
 # php 5.3 doesn't work for Drupal and Aegir.  Downgrade to 5.2 from Karmic (Ubuntu 9.10).
 #   enable Karmic repositories
