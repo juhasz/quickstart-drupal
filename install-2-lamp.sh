13c9687ff9bac89196c66cb380c2965b38981639
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2010-05-07 01:08:43 +0000

    fixes

--- install-2-lamp.sh
+++ install-2-lamp.sh
@@ -4,8 +4,6 @@
 zenity --info --text="Set all passwords to 'quickstart'.  Select 'apache' to configure.  Click 'yes' to phpmyadmin."
 
 # php 5.3 doesn't work for Drupal and Aegir.  Downgrade to 5.2 from Karmic (Ubuntu 9.10).
-#   set default release
-echo 'APT::Default-Release "hardy";' | sudo tee -a /etc/apt/apt.conf.d/01ubuntu > /dev/null
 #   enable Karmic repositories
 echo "# needed sources for php5.2:
 deb http://archive.ubuntu.com/ubuntu      karmic          main restricted universe multiverse
@@ -32,7 +30,6 @@ sudo a2enmod rewrite
 sudo a2dismod cgi
 sudo a2dismod autoindex
 
-# configure php for drupal and modern php development
 # sudo sed -i 's/find_this/replace_with_this/g' infile1 infile2 etc
 sudo sed -i 's/magic_quotes_gpc = On/magic_quotes_gpc = Off/g'     /etc/php5/apache2/php.ini /etc/php5/cli/php.ini
 sudo sed -i 's/short_open_tag = On/short_open_tag = Off/g'         /etc/php5/apache2/php.ini /etc/php5/cli/php.ini
@@ -40,23 +37,20 @@ sudo sed -i 's/max_execution_time = 30/max_execution_time = 300/g' /etc/php5/apa
 sudo sed -i 's/memory_limit = 16M/memory_limit = 64M/g'            /etc/php5/apache2/php.ini /etc/php5/cli/php.ini
 sudo sed -i 's/memory_limit = 32M/memory_limit = 64M/g'            /etc/php5/apache2/php.ini /etc/php5/cli/php.ini
 sudo sed -i 's/;error_log = php_errors.log/error_log = \/var\/log\/php-error.log/g'     /etc/php5/apache2/php.ini /etc/php5/cli/php.ini
-# fix comment bug that will show warning on command line.
-sudo sed -i 's/# /\/\/ /g'            /etc/php5/cli/conf.d/mcrypt.ini
 
-# configure mysql for slow query log
-sudo sed -i 's/#log_slow_queries/log_slow_queries/g'          /etc/mysql/my.conf
-sudo sed -i 's/#long_query_time/long_query_time/g'            /etc/mysql/my.conf
-
-#install APC (op-code cache - improves performance)
-sudo aptitude -y install php-apc
-
-#install xdebug
-sudo aptitude -y install php5-xdebug
+# configure xdebug
 echo "xdebug.remote_enable=on
 xdebug.remote_handler=dbgp
 xdebug.remote_host=localhost
 xdebug.remote_port=9000
-" | sudo tee -a /etc/php5/conf.d/xdebug.ini
+" | sudo tee -a /etc/php5/conf.d/xdebug.ini > /dev/nul
+
+# fix comment bug that will show warning on command line.
+sudo sed -i 's/# /\/\/ /g'            /etc/php5/cli/conf.d/mcrypt.ini
+
+# configure mysql for slow query log
+sudo sed -i 's/#log_slow_queries/log_slow_queries/g'          /etc/mysql/my.cnf
+sudo sed -i 's/#long_query_time/long_query_time/g'            /etc/mysql/my.cnf
 
 # clean up aptitude
 sudo aptitude clean
