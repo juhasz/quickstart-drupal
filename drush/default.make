b080b7a27632d5f779a2e02fa58286df34d526d5
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2010-07-25 17:47:17 +0000

    Avahi fix poormanscron to default.make fix D6 install workflow to remove remembering command

--- drush/default.make
+++ drush/default.make
@@ -2,4 +2,4 @@ core = 6.x
 projects[] = drupal
 projects[] = cck
 projects[] = views
-
+projects[] = poormanscron
--- quickstart-1.prep.sh
+++ quickstart-1.prep.sh
@@ -5,7 +5,14 @@ zenity --info --text="This process requires some manual steps.  Popups like this
 # Fix bug where needed to manually dhcp network
 echo "auto eth0
 iface eth0 inet dhcp" | sudo tee -a /etc/network/interfaces
+
+# Fix bug with Avahi and .local - http://drupal.org/node/822542 - don't completely understand this.  This is for bridged networking?
+sudo sed -i 's/.local/.alocal/g'     /etc/avahi/avahi-daemon.conf
+
+
 sudo /etc/init.d/networking restart
+
+
 
 # Starting size:
 df -h -T > ~/quickstart/quickstart-size-start.txt
