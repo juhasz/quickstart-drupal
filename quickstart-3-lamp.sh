f7f9ae4b6746752587feb2e4399fb6142022445d
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2010-07-25 16:30:53 +0000

    PHP 5.3 default.make downloads poormanscron Manual config note for Radiance theme Manual config note for Selenium IDE firefox plugin

--- quickstart-3-lamp.sh
+++ quickstart-3-lamp.sh
@@ -2,43 +2,51 @@
 
 # Install LAMP
 
-PIN_PHP="php5 php5-dev php5-common php5-xsl php5-curl php5-gd php5-pgsql php5-cli php5-mcrypt php5-sqlite php5-mysql libapache2-mod-php5 php-pear php5-imap"
-PIN_OTHER="php5-xdebug php-apc libapache2-svn"
-
-# php 5.3 doesn't work for Drupal and Aegir.  Downgrade to 5.2 by using LAMP packages from Karmic (Ubuntu 9.10)
-#  Enable Karmic repositories
-echo "# needed sources for php5.2:
-deb http://archive.ubuntu.com/ubuntu      karmic          main restricted universe multiverse
-deb-src http://archive.ubuntu.com/ubuntu  karmic          main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu      karmic-updates  main restricted universe multiverse
-deb-src http://archive.ubuntu.com/ubuntu  karmic-updates  main restricted universe multiverse
-deb http://security.ubuntu.com/ubuntu     karmic-security main restricted universe multiverse
-deb-src http://security.ubuntu.com/ubuntu karmic-security main restricted universe multiverse
-" | sudo tee -a /etc/apt/sources.list.d/karmic.list > /dev/null
-# apt pin php to karmic repositories
-echo '' | sudo tee -a /etc/apt/preferences.d/php-karmic > /dev/null
-for i in $PIN_PHP ; do echo "Package: $i
-Pin: version 5.2.*
-Pin-Priority: 1001
-" | sudo tee -a /etc/apt/preferences.d/php-karmic > /dev/null; done
-echo "Package: php5-xdebug
-Pin: version 2.0.4*
-Pin-Priority: 1001
-" | sudo tee -a /etc/apt/preferences.d/php-karmic > /dev/null
-echo "Package: php-apc
-Pin: version 3.0.*
-Pin-Priority: 1001
-" | sudo tee -a /etc/apt/preferences.d/php-karmic > /dev/null
-echo "Package: libapache2-svn
-Pin: version 1.6.5*
-Pin-Priority: 1001
-" | sudo tee -a /etc/apt/preferences.d/php-karmic > /dev/null
 sudo apt-get update
 zenity --info --text="Set all passwords to 'quickstart'.  Select 'apache' to configure.  Click 'yes' to phpmyadmin."
-sudo apt-get -y install apache2 apache2-threaded-dev mysql-server phpmyadmin $PIN_PHP $PIN_OTHER
-# aptitude hold php.  Aptitude doesn't listen to pinning of apt.  This should prevent 'aptitude safe-upgrade' overwriting php 5.2 with 5.3
-sudo aptitude update
-sudo aptitude hold $PIN_PHP $PIN_OTHER
+LAMP_APACHE="apache2 apache2-threaded-dev libapache2-mod-php5 libapache2-svn"
+LAMP_MYSQL="mysql-server"
+LAMP_PHP="php5 php5-dev php5-common php5-xsl php5-curl php5-gd php5-pgsql php5-cli php5-mcrypt php5-sqlite php5-mysql php-pear php5-imap php5-xdebug php-apc"
+LAMP_TOOLS="phpmyadmin"
+sudo apt-get -y install $LAMP_APACHE $LAMP_MYSQL $LAMP_PHP $LAMP_TOOLS
+
+# NOTE: This has been fixed for Drupal 6.16+ and Aegir 2010-7-17+.  Commenting for now, DELETEME later.
+# # php 5.3 doesn't work for Drupal and Aegir.  Downgrade to 5.2 by using LAMP packages from Karmic (Ubuntu 9.10)
+# PIN_PHP="php5 php5-dev php5-common php5-xsl php5-curl php5-gd php5-pgsql php5-cli php5-mcrypt php5-sqlite php5-mysql libapache2-mod-php5 php-pear php5-imap"
+# PIN_OTHER="php5-xdebug php-apc libapache2-svn"
+# #  Enable Karmic repositories
+# echo "# needed sources for php5.2:
+# deb http://archive.ubuntu.com/ubuntu      karmic          main restricted universe multiverse
+# deb-src http://archive.ubuntu.com/ubuntu  karmic          main restricted universe multiverse
+# deb http://archive.ubuntu.com/ubuntu      karmic-updates  main restricted universe multiverse
+# deb-src http://archive.ubuntu.com/ubuntu  karmic-updates  main restricted universe multiverse
+# deb http://security.ubuntu.com/ubuntu     karmic-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu karmic-security main restricted universe multiverse
+# " | sudo tee -a /etc/apt/sources.list.d/karmic.list > /dev/null
+# # apt pin php to karmic repositories
+# echo '' | sudo tee -a /etc/apt/preferences.d/php-karmic > /dev/null
+# for i in $PIN_PHP ; do echo "Package: $i
+# Pin: version 5.2.*
+# Pin-Priority: 1001
+# " | sudo tee -a /etc/apt/preferences.d/php-karmic > /dev/null; done
+# echo "Package: php5-xdebug
+# Pin: version 2.0.4*
+# Pin-Priority: 1001
+# " | sudo tee -a /etc/apt/preferences.d/php-karmic > /dev/null
+# echo "Package: php-apc
+# Pin: version 3.0.*
+# Pin-Priority: 1001
+# " | sudo tee -a /etc/apt/preferences.d/php-karmic > /dev/null
+# echo "Package: libapache2-svn
+# Pin: version 1.6.5*
+# Pin-Priority: 1001
+# " | sudo tee -a /etc/apt/preferences.d/php-karmic > /dev/null
+# sudo apt-get update
+# zenity --info --text="Set all passwords to 'quickstart'.  Select 'apache' to configure.  Click 'yes' to phpmyadmin."
+# sudo apt-get -y install apache2 apache2-threaded-dev mysql-server phpmyadmin $PIN_PHP $PIN_OTHER
+# # aptitude hold php.  Aptitude doesn't listen to pinning of apt.  This should prevent 'aptitude safe-upgrade' overwriting php 5.2 with 5.3
+# sudo aptitude update
+# sudo aptitude hold $PIN_PHP $PIN_OTHER
 
 zenity --info --text="Choose 'internet' configuration"
 sudo apt-get -y install postfix
