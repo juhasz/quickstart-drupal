8c554d39d7c26d50ef299b168a9712e4269363df
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2010-08-18 04:47:00 +0000

    *** empty log message ***

---
+++ install-oraclexe-amd64.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Database home page can be found here at http://<ip>:8080/apex  username: system
+
+sudo apt-get install libc6-i386
+sudo wget -c http://oss.oracle.com/debian/dists/unstable/main/binary-i386/libaio_0.3.104-1_i386.deb http://oss.oracle.com/debian/dists/unstable/non-free/binary-i386/oracle-xe_10.2.0.1-1.1_i386.deb
+sudo dpkg -i --force-architecture libaio_0.3.104-1_i386.deb
+sudo apt-get install bc
+sudo dpkg -i --force-architecture oracle-xe_10.2.0.1-1.1_i386.deb
+
+sudo /etc/init.d/oracle-xe configure
+
