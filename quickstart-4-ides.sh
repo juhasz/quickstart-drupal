5796a5b43eeb045655174952791b70a94a4075ae
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2010-12-19 23:48:00 +0000

    Tweaks to ides (move netbeans to end cause it has GUI) Tweaks to browsers - launch zenity commands with & so I can still read directions :-)

--- quickstart-4-ides.sh
+++ quickstart-4-ides.sh
@@ -1,31 +1,10 @@
 #!/bin/bash
 
-# Install java - 100mb
+## Install java - 100mb
 sudo apt-get -y install default-jre
 
 
-# Download and install netbeans - 122mb
-NETBEANS_URL="http://download.netbeans.org/netbeans/6.9.1/final/bundles/netbeans-6.9.1-ml-php-linux.sh"
-sudo apt-get -y install wget
-cd ~/Desktop
-wget -O netbeans.sh $NETBEANS_URL
-chmod +x ./netbeans.sh
-zenity --info --text="The Netbeans PHP installer will start now.\n1) Accept the defaults.\n2) Uncheck 'register' on last screen."
-bash ./netbeans.sh
-rm netbeans.sh
-cd ~
-
-
-# Download and install eclipse - 167mb
-ECLIPSE_URL="http://mirrors.ibiblio.org/pub/mirrors/eclipse/technology/epp/downloads/release/helios/SR1/eclipse-php-helios-SR1-linux-gtk.tar.gz"
-wget -O eclipse.tar.gz $ECLIPSE_URL
-tar -xvf eclipse.tar.gz
-sudo ln -s /home/quickstart/eclipse/eclipse /usr/bin/eclipse 
-rm eclipse.tar.gz
-# PPA's are a good idea, but about 458mb!
-#sudo add-apt-repository ppa:yogarine/eclipse/ubuntu && sudo apt-get update
-#sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install eclipse eclipse-pdt eclipse-plugin-cvs eclipse-subclipse
-
+## Basic editors
 
 # Config gedit-2
 gconftool-2 -s /apps/gedit-2/preferences/editor/auto_indent/auto_indent --type=bool true
@@ -42,14 +21,37 @@ sudo apt-get -y install gedit-plugins
 # gnome terminal
 gconftool-2 -s /apps/gnome-terminal/profiles/Default/scrollback_unlimited --type=bool true
 
-
-# Install graphics editors - weights about 25mb
-sudo apt-get -y install gimp
-
 # Install graphical version control - weighs about 58mb
 sudo add-apt-repository ppa:rabbitvcs/ppa && sudo apt-get update
 sudo apt-get -y install rabbitvcs-nautilus
 sudo killall nautilus
 
-# Git GUI?
+# Install graphics editors - weights about 25mb
+sudo apt-get -y install gimp
+
+
+## GUI IDE's
+
+# Download and install eclipse - 167mb
+ECLIPSE_URL="http://mirrors.ibiblio.org/pub/mirrors/eclipse/technology/epp/downloads/release/helios/SR1/eclipse-php-helios-SR1-linux-gtk.tar.gz"
+wget -O eclipse.tar.gz $ECLIPSE_URL
+tar -xvf eclipse.tar.gz
+sudo ln -s /home/quickstart/eclipse/eclipse /usr/bin/eclipse 
+rm eclipse.tar.gz
+# PPA's are a good idea, but about 458mb!
+#sudo add-apt-repository ppa:yogarine/eclipse/ubuntu && sudo apt-get update
+#sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install eclipse eclipse-pdt eclipse-plugin-cvs eclipse-subclipse
+
+# Download and install netbeans - 122mb
+# do this last - it has a GUI :-)
+NETBEANS_URL="http://download.netbeans.org/netbeans/6.9.1/final/bundles/netbeans-6.9.1-ml-php-linux.sh"
+sudo apt-get -y install wget
+cd ~/Desktop
+wget -O netbeans.sh $NETBEANS_URL
+chmod +x ./netbeans.sh
+zenity --info --text="The Netbeans PHP installer will start now.\n1) Accept the defaults.\n2) Uncheck 'register' on last screen."
+bash ./netbeans.sh
+rm netbeans.sh
+cd ~
+
 
--- quickstart-5-browsers.sh
+++ quickstart-5-browsers.sh
@@ -5,16 +5,22 @@
 # Install firefox browser (gecko)
 sudo apt-get -y install firefox flashplugin-nonfree
 
+cd ~
 wget http://www.michaelcole.com/sites/default/files/Quickstart.fbu
 mv Quickstart.fbu profileFx3{default}.fbu
-zenity --info --text="Firefox will start.\n1) Wait for browser to load (firefox is creating Default User).\n2) THEN CLOSE FIREFOX."
+
+zenity --info --text="Firefox will start (creating default profile).\n\nPlease CLOSE FIREFOX. " &
 firefox
+
 firefox -CreateProfile temp
-zenity --info --text="Firefox will start.\n1) Install the FEBE backup extension.\n2) THEN CLOSE FIREFOX."
+
+zenity --info --text="Firefox will start.\n\n1) Please install the FEBE backup extension.\n2) Then CLOSE FIREFOX." &
 firefox -P temp https://addons.mozilla.org/en-US/firefox/downloads/latest/2109/addon-2109-latest.xpi?src=addondetail
-zenity --info --text="Firefox will start.\n1) Tools -> FEBE -> Restore profile.\n2) Click 'Default User' in list (create if it's not there).\n3) 'Select local backup' in the list.\n4) Choose file: ~/profileFx3{default}.fbu.\n5) Start profile restore.\n6) THEN CLOSE FIREFOX"
+
+zenity --info --text="Firefox will start.\n\n1) Tools -> FEBE -> Restore profile.\n2) Click 'default' in list (create if it's not there).\n3) 'Select local backup' in the list.\n4) Choose file: ~/profileFx3{default}.fbu.\n5) Start profile restore.\n6) Ok, ok, then CLOSE FIREFOX" &
 firefox -P temp
-zenity --info --text="Firefox profile manager will start.\n1) Delete temp profile.\n2) THEN CLOSE FIREFOX."
+
+zenity --info --text="Firefox profile manager will start.\n1) Delete temp profile.\n2) THEN CLOSE MANAGER." &
 firefox -ProfileManager
 rm profileFx3{default}.fbu
 
