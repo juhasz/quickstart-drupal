95c91d34fde68b86222dda9fb2a677c21e9727c2
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2011-01-17 03:43:54 +0000

    Add guest additions script.  Move shared folders to 1a-guest

--- quickstart-1-prep.sh
+++ quickstart-1-prep.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-zenity --info --text="These install scripts take several hours.
+zenity --info --text="These install scripts take several hours, and a couple automated reboots.
 
 Towards the end, the process requires some manual steps, guided by popups like this.
 
 This script shouldn't be run more than once."
 
 ## The last password you'll ever need.
+
 # add to sudoers file - careful, this line could brick the box.
 echo "quickstart ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers > /dev/null
+
 
 ## Upgrade
 
@@ -19,23 +21,6 @@ sudo apt-get -y upgrade
 # Install virtual kernel.  Better performance.
 # Removed for 10.10 - http://bugs.launchpad.net/ubuntu/+source/linux/+bug/69224
 #sudo apt-get -y install linux-virtual linux-headers-virtual 
-
-# Install guest additions - dkms recommended on virtualbox.org for upgrade compatibility
-sudo apt-get -y install build-essential linux-headers-$(uname -r)
-sudo apt-get -y install dkms
-sudo apt-get -y install virtualbox-ose-guest-x11
-
-
-## Networking fixes
-# Removed for 10.10 - not a problem anymore - DELETEME FIXME
-
-# Fix bug where needed to manually dhcp network
-#echo "auto eth0
-#iface eth0 inet dhcp" | sudo tee -a /etc/network/interfaces > /dev/null
-# Fix bug with Avahi and .local - http://drupal.org/node/822542 - don't completely understand this.  This is for bridged networking?
-#sudo sed -i 's/.local/.alocal/g'     /etc/avahi/avahi-daemon.conf
-# Restart networking
-#sudo /etc/init.d/networking restart
 
 
 ## Disk size Accounting
---
+++ quickstart-1a-guest.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## install guest additions 
+
+# dkms recommended on virtualbox.org for upgrade compatibility
+sudo apt-get -y install build-essential linux-headers-$(uname -r)
+sudo apt-get -y install dkms
+sudo apt-get -y install virtualbox-ose-guest-x11
+
+# ################################################################################ Shared folder
+
+# Setup shared folders between virtualbox host and virtualbox guest
+# Note difference between shared and vbox-shared.  That's important.  Requires reboot
+sudo sed -i 's/# By default this script does nothing./mount -t vboxsf -o uid=1000,gid=1000 shared \/mnt\/vbox-shared/g'     /etc/rc.local
+sudo mkdir /mnt/vbox-shared
+sudo chmod 777 /mnt/vbox-shared
+cat > /mnt/vbox-shared/readme.txt <<END
+If you are seeing this file, then virtualbox's shared folders are not configured correctly.
+
+1) Power down the Quickstart virtual machine.
+2) On the host computer, start the Virtualbox management UI.
+3) right-click Quickstart -> settings -> shared folders -> click the folder with the green plus on the right
+4) Set the "Folder Path" to a path on the host computer.  Give full read/write access.
+5) Set the "Folder Name" to "shared".  no caps.  no vbox-
+6) Ok -> Ok -> start Quickstart vm and this file should disappear.  
+7) Test by moving a file in the host computer into the host shared folder.
+END
+
