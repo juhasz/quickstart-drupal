37745cd987dab21cdecd3f2ad851b3f47096c1f8
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2011-01-17 03:28:51 +0000

    change reboot strategy

--- install-quickstart.sh
+++ install-quickstart.sh
@@ -10,15 +10,44 @@
 # cvs -z6 -d:pserver:anonymous:anonymous@cvs.drupal.org:/cvs/drupal-contrib checkout -d quickstart contributions/modules/quickstart
 # bash ~/quickstart/install-quickstart.sh
 
+function reboot {
+  echo "gnome-terminal -x bash -c \"~/quickstart/install-quickstart.sh $1\" &" >> ~/.profile
+  echo "*** REBOOTING ***"
+  sleep 2
+  sudo reboot now
+  exit
+}
+
 cd ~
 
-bash -x ~/quickstart/quickstart-1-prep.sh
-bash -x ~/quickstart/quickstart-2-slim.sh
-bash -x ~/quickstart/quickstart-3-lamp.sh
-bash -x ~/quickstart/quickstart-4-ides.sh
+# Undo any previous reboot script
+sudo sed -i 's/gnome-terminal -x bash -c "~\/quickstart\/install-quickstart.sh" &/# deleteme. see install-quickstart.sh /g' ~/.profile
 
-# setup reboot
-echo "gnome-terminal -x bash -c \"~/quickstart/install-quickstart-postreboot.sh\" &" >> ~/.profile
-echo "*** REBOOTING ***"
-sleep 1
-sudo reboot now
+# this switch statement handles reboots.
+case "$1" in
+"")
+  bash -x ~/quickstart/quickstart-1-prep.sh
+  reboot 1a
+  ;;
+"1a")
+  bash -x ~/quickstart/quickstart-1a-guest.sh
+  reboot 2
+  ;;
+"2")
+  bash -x ~/quickstart/quickstart-2-slim.sh
+  bash -x ~/quickstart/quickstart-3-lamp.sh
+  bash -x ~/quickstart/quickstart-4-ides.sh
+  reboot 5
+  ;;
+"5")
+  bash -x ~/quickstart/quickstart-5-browsers.sh
+  bash -x ~/quickstart/quickstart-6-devenv.sh
+  bash -x ~/quickstart/quickstart-7-config.sh
+  firefox ~/quickstart/quickstart-8-manualconfig.txt
+  ;;
+*)
+  echo " *** BAD BAD BAD SOMETHING WENT WRONG!  CALL A DOCTOR! *** "
+  ;;
+esac
+
+
