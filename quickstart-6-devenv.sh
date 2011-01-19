5dbbbe7f7d47934c37894cf7fd1619cef6af0793
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2011-01-19 03:47:53 +0000

    Fix not deleting xhprof Add create project in netbeans for example.dev

--- quickstart-6-devenv.sh
+++ quickstart-6-devenv.sh
@@ -64,23 +64,7 @@ cp ~/quickstart/make_templates/*.make ~/websites
 # Add interesting default document for localhost
 sudo rm /var/www/index.html
 sudo chmod 777 /var/www
-cat > /var/www/index.php <<END
-<html><body>
-  <h1>Drupaldev Quickstart:</h1>
-    <ul>
-      <li><a href='http://localhost/phpmyadmin'>phpmyadmin</a> database editor</li>
-      <li><a href='http://localhost/profiler'>Webgrind profiler</a></li>
-      <li><a href='http://drupal.org/node/788080'>Quickstart documentation</li>
-      <li><a href='http://drupal.org/node/477684'>Drush</a> command line reference</li>
-      <li><a href='http://drupalmodules.com/'>DrupalModules.com</a> and <a href='http://drupal.org/project/usage'>Module Usage Statistics</a></li>
-      <li><a href='http://drupal.org/project/issues?projects=3060&states=8,13,14'>Drupal Patch Queue</a> and <a href='http://drupal.org/patch/review'>Patch reviewing tips</a></li>
-      <li>Configuring <a href='http://wiki.netbeans.org/HowToConfigureXDebug'>xdebug and netbeans</a></li>
-      <li><a href='http://localhost:8080/solr/admin/'>Apache Solr Admin</a></li>
-    </ul>
-  <h1>Current phpinfo():</h1>
-  <?php phpinfo(); ?>
-</body></html>
-END
+cp ~/quickstart/config/index/php /var/www/index.php
 sudo chmod 755 /var/www
 
 
@@ -205,6 +189,7 @@ extension=xhprof.so
 xhprof.output_dir="/home/quickstart/websites/logs/profiler"
 END | sudo tee /etc/php5/conf.d/xhprof.ini > /dev/null
 
+cd ~
 rm  xhprof-0.9.2.tgz
 rm -rf  xhprof-0.9.2
 sudo apache2ctl restart
