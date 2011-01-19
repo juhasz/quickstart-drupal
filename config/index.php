95c98aec3cc00beaae5f5d7780c0f4a74906e646
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2011-01-19 00:13:16 +0000

    Move index.php to config/

---
+++ config/index.php
@@ -0,0 +1,15 @@
+<html><body>
+  <h1>Drupaldev Quickstart:</h1>
+    <ul>
+      <li><a href='http://localhost/phpmyadmin'>phpmyadmin</a> database editor</li>
+      <li><a href='http://localhost/profiler'>Webgrind profiler</a></li>
+      <li><a href='http://drupal.org/node/788080'>Quickstart documentation</li>
+      <li><a href='http://drupal.org/node/477684'>Drush</a> command line reference</li>
+      <li><a href='http://drupalmodules.com/'>DrupalModules.com</a> and <a href='http://drupal.org/project/usage'>Module Usage Statistics</a></li>
+      <li><a href='http://drupal.org/project/issues?projects=3060&states=8,13,14'>Drupal Patch Queue</a> and <a href='http://drupal.org/patch/review'>Patch reviewing tips</a></li>
+      <li>Configuring <a href='http://wiki.netbeans.org/HowToConfigureXDebug'>xdebug and netbeans</a></li>
+      <li><a href='http://localhost:8080/solr/admin/'>Apache Solr Admin</a></li>
+    </ul>
+  <h1>Current phpinfo():</h1>
+  <?php phpinfo(); ?>
+</body></html>
