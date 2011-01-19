ac78b07999bacf77bbd7afe9822be017efb1e76e
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2011-01-19 01:18:04 +0000

    new index.php.  Now with two colors!

--- config/index.php
+++ config/index.php
@@ -1,15 +1,72 @@
-<html><body>
-  <h1>Drupaldev Quickstart:</h1>
-    <ul>
-      <li><a href='http://localhost/phpmyadmin'>phpmyadmin</a> database editor</li>
-      <li><a href='http://localhost/profiler'>Webgrind profiler</a></li>
-      <li><a href='http://drupal.org/node/788080'>Quickstart documentation</li>
-      <li><a href='http://drupal.org/node/477684'>Drush</a> command line reference</li>
-      <li><a href='http://drupalmodules.com/'>DrupalModules.com</a> and <a href='http://drupal.org/project/usage'>Module Usage Statistics</a></li>
-      <li><a href='http://drupal.org/project/issues?projects=3060&states=8,13,14'>Drupal Patch Queue</a> and <a href='http://drupal.org/patch/review'>Patch reviewing tips</a></li>
-      <li>Configuring <a href='http://wiki.netbeans.org/HowToConfigureXDebug'>xdebug and netbeans</a></li>
-      <li><a href='http://localhost:8080/solr/admin/'>Apache Solr Admin</a></li>
-    </ul>
-  <h1>Current phpinfo():</h1>
-  <?php phpinfo(); ?>
-</body></html>
+<html>
+    <body>
+        <h1>Drupal Quickstart Development Environment:</h1>
+        <table width="600" border="1" align="center" cellpadding="2" cellspacing="0">
+          <tr>
+            <td bgcolor="#FFFFee"><strong>Dev Tools on this Virtual Machine:</strong></td>
+            <td bgcolor="#FFFFee"><strong>Quickstart Support and Questions:</strong></td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee"><a href='http://localhost/phpmyadmin'>phpmyadmin</a></td>
+            <td bgcolor="#FFFFee">Tutorials: <a href="http://www.youtube.com/user/drupalquickstart">Youtube videos<img src="http://s.ytimg.com/yt/img/creators_corner/YouTube/youtube_24x24.png" alt="Subscribe to me on YouTube"/></a>
+            
+            </td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee"><a href='http://localhost/profiler'>Webgrind profiler</a></td>
+            <td bgcolor="#FFFFee">Questions: <a href="http://groups.drupal.org/quickstart-drupal-development-environment">Quickstart Group</a></td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee">&nbsp;</td>
+            <td bgcolor="#FFFFee">Patches and bug reports: <a href="http://drupal.org/project/issues/quickstart">Issue Queue</a></td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee">&nbsp;</td>
+            <td bgcolor="#FFFFee">&nbsp;</td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee"><strong>Example Sites in this Virtual Machine:</strong></td>
+            <td bgcolor="#FFFFee"><strong>Quickstart Documentation:</strong></td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee"><a href="http://example.dev">example.dev</a> - Drupal 7</td>
+            <td bgcolor="#FFFFee"><a href="http://drupal.org/project/quickstart">Quickstart Project</a></td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee"><p><a href="example6.dev">example6.dev</a> - Drupal 6</p></td>
+            <td bgcolor="#FFFFee"><a href="http://drupal.org/node/788080">Handbook Documentation</a></td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee">(user admin, password admin)</td>
+            <td bgcolor="#FFFFee">&nbsp;</td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee">&nbsp;</td>
+            <td bgcolor="#FFFFee">&nbsp;</td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee"><strong>Drupal Site Building Documentation:</strong></td>
+            <td bgcolor="#FFFFee"><strong>Drupal Developer Documentation:</strong></td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee"><a href='http://drupal.org/project/usage'>Module Usage Statistics</a></td>
+            <td bgcolor="#FFFFee"><a href="http://api.drupal.org/api/drupal">Drupal API documentation</a></td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee"><a href='http://drupalmodules.com/'>DrupalModules.com</a></td>
+            <td bgcolor="#FFFFee"><a href='http://drupal.org/node/477684'>Drush</a> command line reference</td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee">&nbsp;</td>
+            <td bgcolor="#FFFFee"><a href="http://drupal.org/project/drush_make">Drush Make</a> reference</td>
+          </tr>
+          <tr>
+            <td bgcolor="#FFFFee">&nbsp;</td>
+            <td bgcolor="#FFFFee"><a href='http://drupal.org/project/issues/drupal?text=&status=Open&priorities=All&categories=All&version=7.x&component=All'>Drupal Patch Queue</a> and <a href='http://drupal.org/patch/review'>Patch reviewing tips</a></td>
+          </tr>
+        </table>
+    <h1>Current phpinfo():</h1>
+		<?php phpinfo(); ?>
+		
+</body>
+</html>
