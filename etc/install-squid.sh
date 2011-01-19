6da170b4294c5bf391797778ad5e960ec8d97053
MichaelCole <michaelcole@8121.no-reply.drupal.org>
2011-01-19 19:45:39 +0000

    update squid script

--- etc/install-squid.sh
+++ etc/install-squid.sh
@@ -5,42 +5,9 @@
 # for on-by-default yet.
 
 # Install caching proxy server
+sudo apt-get update
 sudo apt-get -y install squid3
 
-# Configure Ubuntu to use proxy
-sudo iptables -t nat -F # clear table
+echo "http_proxy=\"http://localhost:3128\"" >> /etc/environment
 
-# normal transparent proxy
-sudo iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 80 -j
-REDIRECT --to-port 3127
-
-# handle connections on the same box (SQUIDIP is a loopback instance)
-GID=`id -g proxy`
-sudo iptables -t nat -A OUTPUT -p tcp --dport 80 -m owner --gid-owner
-$GID -j ACCEPT
-sudo iptables -t nat -A OUTPUT -p tcp --dport 80 -j DNAT
---to-destination 127.0.0.1:3127
-
-# Configure proxy to only cache files.drupal.org
-sudo cp /etc/squid3/squid.conf /etc/squid3/squid.conf.old
-echo "
-### Quickstart settings
-
-# servers to cache
-acl drushservers dstdomain ftp.drupal.org
-
-# Tell squid to enter "intercept" mode:
-http_port 3127 intercept
-
-# only cache those servers
-# cache deny all
-cache allow drushservers
-
-# turn on for everybody
-http_access allow all
-#htcp_access allow localhost
-#icp_access allow localhost
-" | sudo tee /etc/squid3/squid.conf > /dev/null
-
-sudo /etc/init.d/squid3 restart
-
+echo "*** REBOOT TO TAKE EFFECT ***"
