f272239f1e4c20b0e4ad1cb23b38d435b95d8718
Michael Cole <drupal@michaelcole.com>
2011-03-21 16:11:59 -0500

    Quick fix

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 cd ~/quickstart
-$QUICKSTART=7.x-0.9beta1
+
+QUICKSTART=7.x-0.9beta1
+
 echo "Updating Quickstart"
-git pull
+git checkout master 2> /dev/null;
+git pull; 
+git checkout $QUICKSTART 2> /dev/null
+
 bash updates/0001.sh
