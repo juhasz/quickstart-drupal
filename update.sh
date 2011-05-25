a63e46edb8dac8bc5cc993b4feee7a7dcc97ed93
quickstart <quickstart@qs091.(none)>
2011-05-25 08:48:42 -0500

    Clean up update script

--- update.sh
+++ update.sh
@@ -1,7 +1,20 @@
 cd ~/quickstart
 
-echo "Updating Quickstart"
-git checkout master 2> /dev/null;
-git pull; 
+QUICKSTART=master
+DRUSH=7.x-4.4
+DRUSH_MAKE=6.x-2.2
 
-bash updates/0001.sh
+# Update Quickstart.  Master is always tag of latest version
+echo "Updating Quickstart"
+git checkout $QUICKSTART 2> /dev/null;
+
+# Update drush
+echo "Updating Drush to $DRUSH"
+cd ~/drush
+git checkout $DRUSH 2> /dev/null
+
+# Update drush make
+echo "Updating Drush Make to $DRUSH_MAKE"
+cd ~/.drush/drush_make
+git checkout $DRUSH_MAKE 2> /dev/null
+
