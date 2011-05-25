5a5b36d3cf8528f65ac0848a5e64ce87b49dd005
quickstart <quickstart@qs091.(none)>
2011-05-25 08:44:15 -0500

    Merged and simplified

--- update.sh
+++ update.sh
@@ -1,10 +1,7 @@
 cd ~/quickstart
 
-QUICKSTART=7.x-0.9beta1
-
 echo "Updating Quickstart"
 git checkout master 2> /dev/null;
 git pull; 
-git checkout $QUICKSTART 2> /dev/null
 
 bash updates/0001.sh
